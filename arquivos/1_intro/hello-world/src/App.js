c4b32edd7925b323eabe823d07346bc792fdc9ab
Victor Moreira <ms.victor.ms@gmail.com>
2023-09-28 16:35:02 -0300

    concluindo o desafio 1

--- arquivos/1_intro/hello-world/src/App.js
+++ arquivos/1_intro/hello-world/src/App.js
@@ -5,6 +5,8 @@ function App() {
   return (
     <div className="App">
       <h1>Hello World React!</h1>
+      <h2>Emmet funcionou</h2>
+      <p>Meu primeiro parágrafo em React!</p>
     </div>
   );
 }
