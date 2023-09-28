93548de9497d997720211c8c9f3cd9f1ec05d3ea
Victor Moreira <ms.victor.ms@gmail.com>
2023-09-28 18:13:03 -0300

    template expressions

--- arquivos/2_fundamentos/fundamentos/src/App.jsx
+++ arquivos/2_fundamentos/fundamentos/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import FirstComponent from './components/FirstComponent'
 import './App.css'
+import TemplateExpressions from './components/TemplateExpressions'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
       <div className="App">
         <h1>Fundamentos React</h1>
         <FirstComponent />
+        <TemplateExpressions />
       </div>
     </>
   )
---
+++ arquivos/2_fundamentos/fundamentos/src/components/TemplateExpressions.jsx
@@ -0,0 +1,18 @@
+const TemplateExpressions = () => {
+
+  const name = "Victor"
+  const data = {
+    age: 27,
+    job: "Programmer",
+  };
+
+  return (
+    <div>
+      <h1>Olá {name}, tudo bem?</h1>
+      <p>Você atua como: {data.job}</p>
+      <p>{4+4}</p>
+    </div>
+  )
+}
+
+export default TemplateExpressions;
