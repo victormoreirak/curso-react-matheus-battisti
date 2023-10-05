0b611958344d1805831eaa22288a6adc77e24ad7
Victor Moreira <victormoreirafin@gmail.com>
2023-10-04 22:37:17 -0300

    renderização de lista em componentes

--- arquivos/3_avancando_em_react/avancando/src/App.jsx
+++ arquivos/3_avancando_em_react/avancando/src/App.jsx
@@ -13,6 +13,12 @@ function App() {
   const name = "Joaquim";
   const [userName] = useState("Maria");
 
+  const car = [
+    { id: 1, brand: "Ferrari", color: "Amarelo", newCar: true, km: 0 },
+    { id: 2, brand: "Kia", color: "Branco", newCar: false, km: 34343 },
+    { id: 3, brand: "Renault", color: "Azul", newCar: false, km: 234 },
+  ]
+
   return (
     <>
       <div className="App">
@@ -35,6 +41,15 @@ function App() {
         {/* reaproveitando */}
         <CarDetails brand="Ford" km={0} color="Vermelha" newCar={true} />
         <CarDetails brand="Fiat" km={4500} color="Branco" newCar={false} />
+        {/* loop em array de objetos */}
+        {car.map((car) => (
+          <CarDetails
+            brand={car.brand}
+            color={car.color}
+            km={car.km}
+            newCar={car.newCar}
+          />
+        ))}
       </div>
     </>
   );
