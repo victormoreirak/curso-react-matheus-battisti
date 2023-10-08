29eb07bf64c3840ecf0111ae2bdf493653c0a745
Victor Moreira <victormoreirafin@gmail.com>
2023-10-08 12:51:41 -0300

    fragment

--- arquivos/3_avancando_em_react/avancando/src/App.jsx
+++ arquivos/3_avancando_em_react/avancando/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { Fragment, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg';
 import './App.css';
@@ -8,9 +8,10 @@ import ListRender from './components/ListRender';
 import ConditionalRender from './components/ConditionalRender';
 import ShowUserName from './components/ShowUserName';
 import CarDetails from './components/CarDetails';
+import Fragment1 from './components/Fragment1';
 
 function App() {
-  const name = "Joaquim";
+  //const name = "Joaquim";
   const [userName] = useState("Maria");
 
   const car = [
@@ -20,38 +21,38 @@ function App() {
   ]
 
   return (
-    <>
-      <div className="App">
-        <h1>Avançando em React</h1>
-        {/* Imagem em public*/}
-        <div>
-          <img src="/img1.jpg" alt="paisagem" />
-        </div>
-        {/* Imagem em Asset */}
-        <div>
-          <img src={City} alt="cidade" />
-        </div>
-        <ManageData />
-        <ListRender />
-        <ConditionalRender />
-        {/* props */}
-        <ShowUserName name={userName} />
-        {/* destructuring */}
-        <CarDetails brand="VW" km={100000} color="Azul" newCar={false} />
-        {/* reaproveitando */}
-        <CarDetails brand="Ford" km={0} color="Vermelha" newCar={true} />
-        <CarDetails brand="Fiat" km={4500} color="Branco" newCar={false} />
-        {/* loop em array de objetos */}
-        {car.map((car) => (
-          <CarDetails
-            brand={car.brand}
-            color={car.color}
-            km={car.km}
-            newCar={car.newCar}
-          />
-        ))}
+    <div className="App">
+      <h1>Avançando em React</h1>
+      {/* Imagem em public*/}
+      <div>
+        <img src="/img1.jpg" alt="paisagem" />
       </div>
-    </>
+      {/* Imagem em Asset */}
+      <div>
+        <img src={City} alt="cidade" />
+      </div>
+      <ManageData />
+      <ListRender />
+      <ConditionalRender />
+      {/* props */}
+      <ShowUserName name={userName} />
+      {/* destructuring */}
+      <CarDetails brand="VW" km={100000} color="Azul" newCar={false} />
+      {/* reaproveitando */}
+      <CarDetails brand="Ford" km={0} color="Vermelha" newCar={true} />
+      <CarDetails brand="Fiat" km={4500} color="Branco" newCar={false} />
+      {/* loop em array de objetos */}
+      {car.map((car) => (
+        <CarDetails
+          brand={car.brand}
+          color={car.color}
+          km={car.km}
+          newCar={car.newCar}
+        />
+      ))}
+      {/* fragment */}
+      <Fragment1 propFragment1="teste" />
+    </div>
   );
 }
 
---
+++ arquivos/3_avancando_em_react/avancando/src/components/Fragment1.jsx
@@ -0,0 +1,11 @@
+const Fragment1 = ({ propFragment1 }) => {
+  return (
+    <>
+      <h2>Primeiro título</h2>
+      <h3>Segundo título</h3>
+      <h4>{propFragment1}</h4>
+    </>
+  );
+};
+
+export default Fragment1;
